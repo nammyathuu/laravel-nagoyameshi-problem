34c5ce17cf42e14759b38a0e733f09211c3c2652
nammyatyuu <namichan2533@yahoo.co.jp>
2024-10-06 14:41:09 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
-    Route::get('users', [Admin\UserController::class, 'index'])->name('home');
+    Route::get('users', [Admin\UserController::class, 'index'])->name('users.index');
+    Route::get('users/{user}', [Admin\UserController::class, 'show'])->name('users.show');
     
 });
